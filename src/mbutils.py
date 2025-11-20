74cc0ad4165ba716fbc9674269eab4f6f833327e
Steve Tell <tell@telltronics.org>
2025-11-19 23:02:58 -0500

    get mb-log fully usable mb-log itself: properly use the logging stuff in mbutils bugfixes to logfile writer stuff in mbutils.py

--- src/mbutils.py
+++ src/mbutils.py
@@ -13,23 +13,21 @@ class ssv_logfile:
         timestr = time.strftime("%y%m%d", tm)
         self.lastday = tm.tm_mday
         logfname = sprintf("%s.%s", self.basename, timestr);
-
         #new_logfile = not(file_exists(logfname))
         self.logfp = open(logfname, "a");
 
     def close(self):
         """close a file"""
-        fprintf(self.logfp, "# closed at %s\n", timelasctime())
+        fprintf(self.logfp, "# closed at %s\n", time.asctime())
         self.logfp.close()
         
     def check_reopen(self):
         """check if its time to rotate the filename, and do so if needed"""
         tm = time.localtime()
-        if(tm.mday != self.lastday):
+        if(tm.tm_mday != self.lastday):
             self.close()
             self.open()
-            self.lastday = tm.tm_mday
-            self.write_headers()
+            self.write_header()
 
     def write_header(self):
         """write headers for all of the columns.  including special hack first,last for our ha data convention"""
@@ -47,7 +45,28 @@ class ssv_logfile:
         for d in datafields:
             fprintf(self.logfp, " %s", d)
             
-        fprintf(self.logfp, " %6.3f", tm.tm_hour+tm.tm_min/60)
+        fprintf(self.logfp, " %7.4f", tm.tm_hour+tm.tm_min/60)
         fprintf(self.logfp, "\n");
         self.logfp.flush()
             
+if __name__ == "__main__":
+    import sys
+    import pprint
+    import argparse
+
+    parser =  argparse.ArgumentParser(description="monbox-config and data store test/demo");
+    parser.add_argument("--verbose", "-v", default=False, action='store_true');
+    parser.add_argument("--test", "-t", default=False, action='store_true');
+
+    logbasename="mylogtest"
+    
+    headers = ["time", "value"]
+    
+    mylog = ssv_logfile(logbasename, headers)
+    mylog.open()
+    mylog.write_header()
+    logfields = [0, 1];     mylog.write_data(logfields)
+    logfields = [1, 2];     mylog.write_data(logfields)
+    mylog.check_reopen()
+    mylog.close()
+    
